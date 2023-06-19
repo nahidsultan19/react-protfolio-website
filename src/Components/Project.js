37bf54b0c5788a7355ed0834a9bc6c2e4d734ecc
nahid <sultannahid38@gmail.com>
2023-06-19 17:09:35 +0800

    changes layout

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -11,17 +11,38 @@ const Project = ({ project }) => {
         navigate(detail);
     }
     return (
-        <div className="card w-3/4 mx-auto max-w-lg bg-black-800 shadow-xl image-full max-h-60" data-aos="fade-up" data-aos-offset="200" data-aos-duration="2000">
-            <figure><img src={img} alt="portfolio4" /></figure>
-            <div className="card-body">
-                <div className="card-actions justify-start">
-                    <button className="btn btn-primary"><a href={url} target="_blank">Live Site</a></button>
-                    <button className='btn' onClick={() => handleProjectDetail(id)}>Details</button>
+        // <div className="card w-auto mx-auto max-w-lg bg-black-800 image-full max-h-60" data-aos="fade-up" data-aos-offset="200" data-aos-duration="2000">
+        //     <figure><img className='w-auto h-5' src={img} alt="portfolio4" /></figure>
+        //     <div className="card-body">
+        //         <div className="card-actions justify-start">
+        //             <button className="btn btn-primary"><a href={url} target="_blank">Live Site</a></button>
+        //             <button className='btn' onClick={() => handleProjectDetail(id)}>Details</button>
+        //         </div>
+        //         <h2 className="card-title">{name}</h2>
+        //         {/* <p>{des.slice(0, 100)}</p> */}
+        //     </div>
+        // </div>
+        <div className="mx-auto max-w-sm rounded overflow-hidden shadow-lg">
+            <div className="relative">
+                <img
+                    className="w-full h-48 object-cover"
+                    src={img}
+                    alt="Project Background"
+                />
+                <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
+                <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
+                    <h2 className="text-white text-xl font-bold">{name}</h2>
                 </div>
-                <h2 className="card-title">{name}</h2>
-                <p>{des.slice(0, 100)}</p>
+            </div>
+            <div className="px-6 py-4">
+                <p className="text-gray-700 text-base pb-4">
+                    {des.slice(0, 36)}
+                </p>
+                <button className="btn btn-primary"><a href={url} target="_blank">Live Site</a></button>
+                <button className='btn ml-2' onClick={() => handleProjectDetail(id)}>Details</button>
             </div>
         </div>
+
     );
 };
 
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -10,8 +10,8 @@ const Projects = () => {
     }, [])
     return (
         <div id='project'>
-            <h2 className='text-center text-3xl uppercase font-bold text-purple-500 my-4'>My Simple Portfolio Project</h2>
-            <div className='grid grid-cols-1 md:grid-cols-2 py-12 lg:mx-20  gap-5'>
+            <h2 className='text-center text-3xl font-bold text-gradient-to-r text-slate-100 my-4 py-12'>My Simple Portfolio Project</h2>
+            <div className='md:flex flex-row px-10 pb-16'>
                 {projects.map(project => <Project key={project.id} project={project}></Project>)}
             </div>
         </div>
