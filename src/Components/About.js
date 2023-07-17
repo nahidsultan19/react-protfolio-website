937d6472cea20bd1c576eeef51841c0585396ba2
nahid <sultannahid38@gmail.com>
2023-07-17 18:04:02 +0800

    no changes

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,12 +3,12 @@ import profile from '../assets/images/profile.png';
 
 const About = () => {
     return (
-        <div className='lg:flex justify-center items-center sm:px-10 text-white' id='about'>
+        <div className='md:flex  justify-center items-center sm:px-10 text-white' id='about'>
             <div className=''>
                 <img src={profile} alt="" className='mask mask-circle p-10 lg:max-w-sm md:w-96 sm:mx-auto bg-indigo-200' data-aos="fade-down" data-aos-offset="200" data-aos-duration="1000" />
             </div>
-            <div className='lg:py-12 ml-10 pb-5 max-w-md  sm:pe-4' data-aos="fade-left" data-aos-offset="200" data-aos-duration="2000">
-                <h2 className='text-2xl font-bold'>About Me</h2>
+            <div className='lg:py-12 ml-10 pb-5 max-w-md  sm:pe-4'>
+                <h2 className='text-2xl font-bold lg:mt-5 mt-10'>About Me</h2>
                 <div className='bg-gradient-to-r from-orange-500 to-orange w-1/4 h-0.5'></div>&nbsp;
                 <p className='text-justify'>I am an enthusiastic Software Developer, with a few years of experience in Frontend Technology. I specialize in building user-friendly and performant web applications using modern frontend technologies. I'm proficient in React.js and its ecosystem.
                     I have some portfolio projects similar to this technology. Also, I have developed the MERN Stack project. Looking for a position where I can explore my knowledge.
